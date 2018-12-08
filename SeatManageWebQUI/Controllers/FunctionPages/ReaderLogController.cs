7c04ea69aa74414ee733e6904f84dc79010d1741
Administrator <Administrator@DESKTOP-7IJ3V0H>
2018-12-08 18:41:02 +0800

    update

--- SeatManageWebQUI/Controllers/FunctionPages/ReaderLogController.cs
+++ SeatManageWebQUI/Controllers/FunctionPages/ReaderLogController.cs
@@ -1,6 +1,8 @@
-﻿using SeatManageWebV5.Code;
+﻿using SeatManage.EnumType;
+using SeatManageWebV5.Code;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -16,17 +18,56 @@ namespace SeatManageWebQUI.Controllers.FunctionPages
             return View();
         }
 
-        public ActionResult SelectBespeakLog()
+        public string GetGridString(string beginDateString,string endDateString,string statusString)
+        {
+            string cardNo = this.LoginId;
+            if (string.IsNullOrEmpty(cardNo))
+            {
+                cardNo = this.LoginId;
+            }
+            BookingStatus status = BookingStatus.None;
+            if (statusString != null)
+            {
+                status = (BookingStatus)int.Parse(statusString);
+            }
+            DateTime startDate = string.IsNullOrEmpty(beginDateString)? DateTime.Now.Date: DateTime.Parse(beginDateString);
+            DateTime endDate = string.IsNullOrEmpty(endDateString) ? DateTime.Now.AddDays(7).Date : DateTime.Parse(endDateString); ;//dpEndDate.SelectedDate.Value;
+            List<BookingStatus> statusList = new List<BookingStatus>();
+            if (status == BookingStatus.None)
+            {
+                statusList.Add(BookingStatus.Cencaled);
+                statusList.Add(BookingStatus.Confinmed);
+                statusList.Add(BookingStatus.Waiting);
+            }
+            else
+            {
+                statusList.Add(status);
+            }
+            DataTable dt = LogQueryHelper.BespeakLogQuery(cardNo, null, statusList, startDate, endDate.AddHours(23).AddMinutes(59));
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{");
+            sb.Append("\"form.paginate.pageNo\": 1,");
+            sb.Append("\"form.paginate.totalRows\": 100,");
+            sb.Append("	\"rows\": [");
+            foreach (DataRow r in dt.Rows)
+            {
+                sb.Append("{\"ReadingRoomName\": '" + r["ReadingRoomName"] + "',\"SeatNum\": \"" + r["SeatNum"] + "\",\"BsepeakState\": \"" + r["BsepeakState"] + "\",\"SubmitTime\": \"" + r["SubmitTime"] + "\",\"BespeakTime\": \"" + r["BespeakTime"] + "\",\"CancelTime\": \"" + r["CancelTime"] + "\",\"Remark\": \"" + r["Remark"] + "\"}");
+                sb.Append(",");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append("]");
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+
+        public ActionResult SelectBespeakLog(string beginDateString, string endDateString, string statusString)
         {
             string nowDay = DateTime.Now.ToShortDateString();
             string after7Day = DateTime.Now.AddDays(7).ToShortDateString();
             ViewBag.nowDay = nowDay;
             ViewBag.after7Day = after7Day;
-            return View();
-        }
 
-        public string GetBespeakEnumKeyValue()
-        {
             List<BespeakEnumKey_Value> list = new List<BespeakEnumKey_Value>();
             list.Add(new BespeakEnumKey_Value() { BespeakState = SeatManage.SeatManageComm.SeatComm.ConvertBookingStatus(SeatManage.EnumType.BookingStatus.None), Value = ((int)SeatManage.EnumType.BookingStatus.None).ToString() });
             list.Add(new BespeakEnumKey_Value() { BespeakState = SeatManage.SeatManageComm.SeatComm.ConvertBookingStatus(SeatManage.EnumType.BookingStatus.Waiting), Value = ((int)SeatManage.EnumType.BookingStatus.Waiting).ToString() });
@@ -34,15 +75,20 @@ namespace SeatManageWebQUI.Controllers.FunctionPages
             list.Add(new BespeakEnumKey_Value() { BespeakState = SeatManage.SeatManageComm.SeatComm.ConvertBookingStatus(SeatManage.EnumType.BookingStatus.Cencaled), Value = ((int)SeatManage.EnumType.BookingStatus.Cencaled).ToString() });
 
             StringBuilder sb = new StringBuilder();
-            sb.Append("{\"list\":[");
+
+            sb.Append("<select prompt=\"预约状态\"  id=\"selStatus\" name=\"selStatus\">");
+
             foreach (var item in list)
             {
-                sb.Append("{\"key\":\"" + item.Value + "\",\"value\":\"" + item.BespeakState + "\"},");
+                sb.Append("<option value=\""+item.Value+"\">"+item.BespeakState+"</option>");
             }
-            sb.Append("]}");
-
-            return sb.ToString();
+            sb.Append("</select>");
+            ViewBag.Data = GetGridString(beginDateString, endDateString, statusString);
+            ViewBag.BespeakEnumKeyValue = sb.ToString();
+            return View();
         }
+
+
 
         public ActionResult SelectEnterOutLog()
         {
