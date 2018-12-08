1c832ecc73614a0dedb815c244833ff0228dc586
Administrator <Administrator@DESKTOP-7IJ3V0H>
2018-12-08 16:53:50 +0800

    start working at reader log

--- SeatManageWebQUI/Controllers/FunctionPages/ReaderLogController.cs
+++ SeatManageWebQUI/Controllers/FunctionPages/ReaderLogController.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using SeatManageWebV5.Code;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
 namespace SeatManageWebQUI.Controllers.FunctionPages
 {
-    public class ReaderLogController : Controller
+    public class ReaderLogController : BaseController
     {
         // GET: ReaderLog
         public ActionResult Index()
@@ -16,7 +18,30 @@ namespace SeatManageWebQUI.Controllers.FunctionPages
 
         public ActionResult SelectBespeakLog()
         {
+            string nowDay = DateTime.Now.ToShortDateString();
+            string after7Day = DateTime.Now.AddDays(7).ToShortDateString();
+            ViewBag.nowDay = nowDay;
+            ViewBag.after7Day = after7Day;
             return View();
+        }
+
+        public string GetBespeakEnumKeyValue()
+        {
+            List<BespeakEnumKey_Value> list = new List<BespeakEnumKey_Value>();
+            list.Add(new BespeakEnumKey_Value() { BespeakState = SeatManage.SeatManageComm.SeatComm.ConvertBookingStatus(SeatManage.EnumType.BookingStatus.None), Value = ((int)SeatManage.EnumType.BookingStatus.None).ToString() });
+            list.Add(new BespeakEnumKey_Value() { BespeakState = SeatManage.SeatManageComm.SeatComm.ConvertBookingStatus(SeatManage.EnumType.BookingStatus.Waiting), Value = ((int)SeatManage.EnumType.BookingStatus.Waiting).ToString() });
+            list.Add(new BespeakEnumKey_Value() { BespeakState = SeatManage.SeatManageComm.SeatComm.ConvertBookingStatus(SeatManage.EnumType.BookingStatus.Confinmed), Value = ((int)SeatManage.EnumType.BookingStatus.Confinmed).ToString() });
+            list.Add(new BespeakEnumKey_Value() { BespeakState = SeatManage.SeatManageComm.SeatComm.ConvertBookingStatus(SeatManage.EnumType.BookingStatus.Cencaled), Value = ((int)SeatManage.EnumType.BookingStatus.Cencaled).ToString() });
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{\"list\":[");
+            foreach (var item in list)
+            {
+                sb.Append("{\"key\":\"" + item.Value + "\",\"value\":\"" + item.BespeakState + "\"},");
+            }
+            sb.Append("]}");
+
+            return sb.ToString();
         }
 
         public ActionResult SelectEnterOutLog()
