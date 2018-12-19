b0c66c22d3593f425082b2bf4130a0a5416abb87
Administrator <Administrator@DESKTOP-7IJ3V0H>
2018-12-19 23:41:59 +0800

    update

--- SeatManageWebQUI/Controllers/FunctionPages/StatisticalController.cs
+++ SeatManageWebQUI/Controllers/FunctionPages/StatisticalController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace SeatManageWebQUI.Controllers.FunctionPages
 {
-    public class StatisticalController : Controller
+    public class StatisticalController : BaseController
     {
         // GET: Statistical
         public ActionResult Index()
