fc06ddd9a50e69168f1da7f17849d0f9d60fab60
Administrator <Administrator@PC-20181021JRDG>
2019-01-02 15:20:45 +0800

    123

---
+++ SeatManageWebQUI/Controllers/FunctionPages/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeatManageWebQUI.Controllers.FunctionPages
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestLoading()
+        {
+
+            System.Threading.Thread.Sleep(5000);
+            return View();
+        }
+    }
+}
