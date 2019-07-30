2290a47d4cc00a6b81d0b55b4928ed2fb7179aa5
duncan <duncan@DESKTOP-TODNB4U>
2019-07-30 23:42:22 +0800

    新增了空间系统几个服务程序

---
+++ SpaceBookingTimerMonitor.WCFHost/SpaceBookingTimerMonitorWCFHost.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBookingTimerMonitor.WCFHost
+{
+    public class SpaceBookingTimerMonitorWCFHost : IService.IService
+    {
+
+        public override string ToString()
+        {
+            return "空间预约系统定时监控服务";
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("监控服务开始工作");
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SpaceDeviceAccess.WCFHost/SpaceDeviceAccessWCFHost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceDeviceAccess.WCFHost
+{
+    public class SpaceDeviceAccessWCFHost : IService.IService
+    {
+        public override string ToString()
+        {
+            return "空间预约系统设备获取程序";
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("空间预约系统设备获取程序   开始工作");
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
